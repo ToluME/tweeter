eebfb854cf24915d8a7070a010abd9b5734ea84e
Tolu Eshiet <tmeshiet@gmail.com>
2023-07-13 10:52:59 -0600

    Added the input length counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $('.new-tweet textarea').on('keyup', function(event) {
+    event.preventDefault();
+
+    const maxLength = 140; // Maximum character limit for the tweet
+    const inputValue = $(this).val(); // Gets textarea value
+    const inputLength = inputValue.length; // Gets the length of the input value
+
+    // Update the character counter
+    const remainingChars = maxLength - inputLength;
+    const counterElement = $(this).closest('.new-tweet').find('.counter');
+    counterElement.text(remainingChars);
+
+    /// Add red color to the counter when input is invalid
+    if (inputLength > maxLength) {
+      counterElement.addClass('invalid');
+    } else {
+      counterElement.removeClass('invalid');
+    }
+    // Log the input value and length
+    console.log('Textarea value:', inputValue);
+    console.log('Inpute Length:', inputLength);
+  });
+});
